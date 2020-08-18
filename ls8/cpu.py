76d753123e37d2fa186f66da60e219451571a222
ksemenza <ksemenza13@gmail.com>
2020-08-18 12:04:35 -0400

    MVP Day 2 complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,12 @@
 
 import sys
 
+LDI = 0b10000010
+HLT = 0b00000001
+PRN = 0b01000111
+MUL = 0b10100010
+NOP = 0b00000000
+
 """ 
 #TODO LDI: Load Immediately, store values to register OR set register to values
  loading puts into a particular defined register to a specific value defined instructions
@@ -14,46 +20,36 @@ Raw print file created by a "print to file"
 Assembly language instructions to stop if system enters an idle state
 Does not continue until the next external interrput is fired
 
+#TODO MUL Unsigned Multiply 
+Multiplies 8-, 16-, or 32 format reg8/mem8, reg16/mem16, reg32/mem32
+
+#TODO NOP No Operations
+Commonly used for timing purposes, aligning memory, prevent hazards, create branch delay slot,
+render existing instruction void
+
+
 """
 
 class CPU:
     """Main CPU class."""
-#TODO Constants
-    LDI = 0b10000010
-    HLT = 0b00000001
-    PRN = 0b01000111
-    
-    
     def __init__(self):
-        """Construct a new CPU."""
-#TODO Implement CPU Constructor
+    #TODO Implement CPU Constructor
         self.ram = [0] * 256
         self.register = [0] * 8
         self.pc = 0
-#TODO Running state
         self.running = True
-        pass
 
     def load(self):
-        """Load a program into memory."""
+        filename = sys.argv[1]
 
-        address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(filename) as f:
+            for address, line in enumerate(f):
+                line = line.split('#')
+                try:
+                    v = int(line[0], 2)
+                except ValueError:
+                    continue
+                self.ram_write(address, v)
 
 
     def alu(self, op, reg_a, reg_b):
@@ -61,6 +57,9 @@ class CPU:
 
         if op == "ADD":
             self.register[reg_a] += self.register[reg_b]
+    # TODO add MUL function
+        elif op == "MUL":
+            self.register[reg_a] *= self.register[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -85,30 +84,55 @@ class CPU:
 
         print()
 
-#TODO initate running 
+#TODO Created instructional functions to replace hard-coding to use with callback 
+    def LDI(self):
+        register_num = self.ram_read(self.pc + 1)
+        value = self.ram_read(self.pc + 2)
+        self.register[register_num] = value
+        self.pc += 3
+
+    def HLT(self):
+        self.running = False
+    
+    def PRN(self):
+        register_num = self.ram_read(self.pc + 1)
+        print(self.register[register_num])
+        self.pc += 2
+    
+    def MUL(self):
+        register_num1 = self.ram_read(self.pc + 1)
+        register_num2 = self.ram_read(self.pc + 2)
+        self.alu("MUL", register_num1, register_num2)
+        self.pc += 3
+
+    def NOP(self):
+        self.pc += 1
+
+    def cb_func(self, n):
+        branch_table = {
+            NOP : self.NOP,
+            HLT : self.HLT,
+            PRN : self.PRN,
+            LDI : self.LDI,
+            MUL : self.MUL
+        }
+
+        f = branch_table[n]
+        if branch_table.get(n) is not None:
+            f()
+        else:
+            print(f'Unknown instruction {n} at address {self.pc}')
+            sys.exit(1)
+
+#TODO Replaced hard-coded functions with callback functions
     def run(self):
         while self.running:
             reg_instructor = self.ram_read(self.pc)
-            if reg_instructor == self.LDI:
-                register_num = self.ram_read(self.pc + 1)
-                value = self.ram_read(self.pc + 2)
-                self.ram_write(register_num, value)
-                self.pc += 3
-            elif reg_instructor == self.HLT:
-                self.running = False
-            elif reg_instructor == self.PRN:
-                register_num = self.ram[self.pc + 1]
-                print(self.ram[register_num])
-                self.pc += 2
-            else:
-                print(f'Error: Instruction {reg_instructor} at address {self.pc}')
-                sys.exit(1)
+            self.cb_func(reg_instructor)
+            
 
-#TODO Add read/write RAM functions
-    #2-1 create ram_read 
     def ram_read(self, address):
         return self.ram[address]
     
-    #2-2 create ram_write
     def ram_write(self, address, value):
         self.ram[address] = value
